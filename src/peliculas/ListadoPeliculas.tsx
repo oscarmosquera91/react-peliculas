e1415d6d6294be88590717ee1f67d6b798e33501
oscarmosquera91 <oscar.mosquera91@hotmail.com>
2023-09-11 13:04:58 -0500

    58 Loistado generico

--- src/peliculas/ListadoPeliculas.tsx
+++ src/peliculas/ListadoPeliculas.tsx
@@ -2,23 +2,24 @@ import PeliculaIndividual from "./PeliculaIndividual";
 import { pelicula } from "./peliculas.model";
 import css from "./ListadoPeliculas.module.css"
 import Cargando from "../utils/Cargando";
+import ListadoGenerico from "../utils/ListadoGenerico";
 
 export default function ListadoPeliculas(props: listadoPeliculasProps){
 
-  if(!props.peliculas){
-    return <Cargando/>
-  } else if(props.peliculas.length === 0){
-    return <>No hay elementos para mostrar</>
-  } else {
+ 
     return(
-      <div className={css.div}>
+      <ListadoGenerico 
+      cargandoUI={<>Cargando...</>}
+      listado={props.peliculas}>
+        <div className={css.div}>
         {props.peliculas?.map(pelicula =>
         <PeliculaIndividual pelicula={pelicula}
         key={pelicula.id}
         />)}
       </div>
+      </ListadoGenerico>
     )
-  }
+  
 
  
 
