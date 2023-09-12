98ed1776dbce8459015a266647c944cad14cd26b
oscarmosquera91 <oscar.mosquera91@hotmail.com>
2023-09-12 09:45:36 -0500

    63 configurando Rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,29 @@
-import React, { useEffect, useState } from 'react';
 import './App.css';
-import { landingPageDTO, pelicula } from './peliculas/peliculas.model';
-import ListadoPeliculas from './peliculas/ListadoPeliculas';
-import Button from './utils/Button';
 import Menu from './utils/Menu';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import IndiceGeneros from './generos/IndiceGeneros';
+import LandingPages from './LandingPages';
 
 function App() {
 
-  const [peliculas, setPeliculas] = useState<landingPageDTO>({})
-
-  useEffect(() => {
-    const timerId = setTimeout(()=> {
-      setPeliculas({enCartelera: [
-        {
-          id: 1, titulo: 'Spider-Man: Far from Home', 
-          poster: 'https://m.media-amazon.com/images/M/MV5BZGVmMDJlOWYtODQxZS00YWFlLWFmYTYtZmExMWY5NWE5NjEwXkEyXkFqcGdeQXVyODc0OTEyNDU@._V1_.jpg'
-        },
-        {
-          id: 2, titulo: 'Moana', 
-          poster: 'https://m.media-amazon.com/images/M/MV5BNTNhYTI0OGQtYmZiZC00NTI4LTgzMDUtYmQwMzJkZmUwNzYzXkEyXkFqcGdeQXVyNjg2NjgzMjM@._V1_FMjpg_UY800_.jpg'
-        }
-      ],
-    proximosExtrenos: [
-      {
-        id: 1, titulo: 'Spider-Man: Far from Home', 
-        poster: 'https://m.media-amazon.com/images/M/MV5BNDIyNjc1MjgtY2ViNS00N2EwLThmNWQtZDJlYjBiYmI5MTg0XkEyXkFqcGdeQXVyNzI1NzMxNzM@._V1_QL75_UX380_CR0,0,380,562_.jpg'
-      }
-    ]})
-
-    }, 1000);
-    return () => clearTimeout(timerId)
-  })
-
- 
-
   return (
     <>
-    <Menu/>
-    <div className='container'>
+    <BrowserRouter>
+      <Menu/>
+      <div className='container'>
+        <Switch>
+          <Route exact path="/"> 
+            <LandingPages/>
+          </Route>
 
-      <h3>En Cartelera</h3>
-      <ListadoPeliculas peliculas={peliculas.enCartelera}/>
-
-      <h3>Proximos Estrenos</h3>
-      <ListadoPeliculas peliculas={peliculas.proximosExtrenos}/>
-    </div>
+          <Route path="/">
+            <IndiceGeneros/>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+    
+    
     
     </>
     
--- src/utils/Menu.tsx
+++ src/utils/Menu.tsx
@@ -1,20 +1,22 @@
+import { NavLink } from "react-router-dom";
 
 export default function Menu(){
   const claseActiva = "active";
   return(
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <NavLink className="navbar-brand" activeClassName={claseActiva} 
+         to="/">
           React Peliculas
-        </a>
+        </NavLink>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a
-                className="nav-link"
-                href="/generos"              >
+              <NavLink
+                className="nav-link" activeClassName={claseActiva} 
+                to="/generos">
                 Generos
-              </a>
+              </NavLink>
             </li>            
           </ul>
         </div>
