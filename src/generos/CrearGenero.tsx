0684fdd2d53a4f2b33dbf0f9a918c29cd15331a7
oscarmosquera91 <oscar.mosquera91@hotmail.com>
2023-09-13 00:52:23 -0500

    74 Desativando en boton salvar

--- src/generos/CrearGenero.tsx
+++ src/generos/CrearGenero.tsx
@@ -1,38 +1,43 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import Button from "../utils/Button";
-import {  Form, Formik } from 'formik';
-import * as Yup from 'yup';
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
 import FormGroupText from "../utils/FormGroupText";
 
-export default function CrearGenero(){
+export default function CrearGenero() {
   // const history =useHistory();
   return (
     <>
-    <h3>Crear Genero</h3>
+      <h3>Crear Genero</h3>
 
-    <Formik initialValues={{
-      nombre: ''
-    }} 
-      onSubmit={values => {
-        console.log(values)
-    }}
+      <Formik
+        initialValues={{
+          nombre: "",
+        }}
 
-    validationSchema={Yup.object({
-      nombre: Yup.string().required('Este campo es requerido').primeraLetraMayuscula()
-    })}
+        onSubmit={async values => {
+          await new Promise(r => setTimeout(r, 100));
+          console.log(values);
+        }}
 
-    >
-      <Form>
-        <FormGroupText campo="nombre" label="Nombre" />
+        validationSchema={Yup.object({
+          nombre: Yup.string()
+            .required("Este campo es requerido")
+            .primeraLetraMayuscula(),
+        })}
+      >
+        {(formikProps) => (
+          <Form>
+            <FormGroupText campo="nombre" label="Nombre" />
 
-        <Button type="submit" >Salvar</Button>
-        <Link className="btn btn-secondary" to="/generos" >Cancelar</Link>
-      </Form>
-
-    </Formik>
-
-    
+            <Button disabled={formikProps.isSubmitting}
+             type="submit">Salvar</Button>
+            <Link className="btn btn-secondary" to="/generos">
+              Cancelar
+            </Link>
+          </Form>
+        )}
+      </Formik>
     </>
-    
-  )
+  );
 }
--- src/utils/Button.tsx
+++ src/utils/Button.tsx
@@ -3,6 +3,7 @@ import { ReactElement } from "react";
 export default function Button(props: buttonProps){
 return(
   <button type={props.type} className="btn btn-primary"
+  disabled={props.disabled}
   onClick={props.onClick}
   >{props.children}</button>
 )
@@ -13,8 +14,10 @@ interface buttonProps{
   children: React.ReactNode,
   onClick?(): void, //Voi para indicar que no debe retornar nada
   type: "button" | "submit"
+  disabled: boolean,
 }
 
 Button.defaultProps = {
-  type:  "button"
+  type:  "button",
+  disabled:  false
 }
